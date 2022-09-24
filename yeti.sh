6c990dae612b3f27739e78f8bd81e80f3359a3d8
BasonJoel <77477928+BasonJoel@users.noreply.github.com>
2022-09-25 11:38:22 +1300

    Update yeti.sh

--- yeti.sh
+++ yeti.sh
@@ -6,7 +6,6 @@ wget https://raw.githubusercontent.com/BasonJoel/things/main/requirements.txt
 cd ~/yeti
 docker-compose -p yeti -f extras/docker/dev/docker-compose.yaml run -d -p 8080:8080 yeti /docker-entrypoint.sh uwsgi-http > dockertmp.txt
 docker cp extras/docker/dev/requirements.txt  $(cat dockertmp.txt):/opt/yeti/requirements.txt
-docker exec $(cat dockertmp.txt) /bin/bash -c "apt-get update && apt-get install cmdtest -y"
 docker restart $(cat dockertmp.txt)
 docker exec $(cat dockertmp.txt) /bin/sh -c "cp yeti.conf yeti.conf.sample && pip3 install -r requirements.txt && yarn install && mv yeti.conf.sample yeti.conf && sed -i '35s/# host = 127.0.0.1/host = mongodb/' yeti.conf && sed -i '49s/# host = 127.0.0.1/host = redis/' yeti.conf"
 echo yeti started on http://localhost:8080
